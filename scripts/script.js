53fa3ac37253fd8e66bdb206e52e27f4f2b43b82
Michael <Magawrych85@gmail.com>
2024-09-27 23:29:14 -0400

    push again

---
+++ scripts/script.js
@@ -0,0 +1,18 @@
+// Cache at least one element using selectElementById
+
+let head1 = document.getElementById('heading1');
+
+head1.textContent = `New Title`;
+
+document.getElementById("head2").style.color = "blue";
+
+
+// Cache at least one element using querySelector or querySelectorAll
+
+let body = document.querySelector('body');
+
+body.style.backgroundColor = 'teal';
+
+
+// Use the parent-child-sibling relationship to navigate between elements at least once (firstChild, lastChild, parentNode, nextElementSibling, etc.)
+
