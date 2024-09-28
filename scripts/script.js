5b7e9bb488935584b517d11c49c99049baa92e76
Michael <Magawrych85@gmail.com>
2024-09-28 00:16:14 -0400

    push again

--- scripts/script.js
+++ scripts/script.js
@@ -4,7 +4,7 @@ let head1 = document.getElementById('heading1');
 
 head1.textContent = `New Title`;
 
-document.getElementById("head2").style.color = "blue";
+document.getElementById("head2").style.color = `blue`;
 
 
 // Cache at least one element using querySelector or querySelectorAll
@@ -15,4 +15,8 @@ body.style.backgroundColor = 'teal';
 
 
 // Use the parent-child-sibling relationship to navigate between elements at least once (firstChild, lastChild, parentNode, nextElementSibling, etc.)
+
+let listItem2 = document.getElementById("item1").nextSibling;
+
+listItem2.textContent = `Hello`;
 
